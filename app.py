491c9b0d4780358480ee6ee9d07844dcbd5a516a
Américo Valdazo <63754514+ValdazoAmerico@users.noreply.github.com>
2023-08-01 09:28:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -73,8 +73,9 @@ with col1:
 	            try:
 	                    docs = vectordb.as_retriever().get_relevant_documents(user_input)
 	                    raw_string = ''
-	                    for d in docs:
-	                    	raw_string += d.page_content.replace('\n', ' ')
+	                    for d in range(len(docs)):
+				raw_string += f'Extracto {d+1}:\n'
+	                    	raw_string += docs[d].page_content.replace('\n', ' ')
 	                    	raw_string += '\n\n'
 	                    st.session_state.data.append(raw_string)
 	                    output = chain({"question":user_input})['answer']
@@ -97,5 +98,5 @@ with col2:
 
 		# Display the EHR patient data
 		if patient_data:
-			st.subheader(f"Context Information:")
+			st.subheader("Información de contexto:")
 			stx.scrollableTextbox(patient_data,height = 350)
