262a375d80a39af9a09f1a6a9e2786951355754e
Américo Valdazo <63754514+ValdazoAmerico@users.noreply.github.com>
2023-08-15 10:28:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -124,9 +124,8 @@ with col1:
 	            try:
 	                    docs = retriever.get_relevant_documents(user_input)
 	                    if len(st.session_state.ai) == 0:
-	                        response = chain({"question":user_input, "chat_history":[]})
 	                        with get_openai_callback() as cb:
-	                        	result = chain({"question": "cuales son las indicaciones para colocar un resincronizador ?", "chat_history": []})
+	                        	response = chain({"question": user_input, "chat_history": []})
 	                        print("CB:", cb)
 				output = response['answer']
 	                        docs = response['source_documents']
@@ -143,7 +142,9 @@ with col1:
 	                        st.session_state['generated'].append(output)   
 	                    elif len(st.session_state.ai) == 1:
 	                        chat_history = [(st.session_state['past'][-1], st.session_state['generated'][-1])]
-	                        response = chain({"question": user_input, "chat_history": chat_history})
+	                        with get_openai_callback() as cb:
+	                        	response = chain({"question": user_input, "chat_history": chat_history})
+	                        print("CB:", cb)
 	                        output = response['answer']
 	                        docs = response['source_documents']   
 	                        raw_string = ''
@@ -159,7 +160,9 @@ with col1:
 	                        st.session_state['generated'].append(output)
 	                    else:
 	                        chat_history = [(st.session_state['past'][-2], st.session_state['generated'][-2]), (st.session_state['past'][-1], st.session_state['generated'][-1])]
-	                        response = chain({"question": user_input, "chat_history": chat_history})
+	                        with get_openai_callback() as cb:
+	                        	response = chain({"question": user_input, "chat_history": chat_history})
+	                        print("CB:", cb)
 	                        output = response['answer']
 	                        docs = response['source_documents']
 	                        raw_string = ''
