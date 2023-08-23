55944d6a060c71b2264e68e4086ae59eed6aa232
Américo Valdazo <63754514+ValdazoAmerico@users.noreply.github.com>
2023-08-23 11:20:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,78 +36,84 @@ if "temp" not in st.session_state:
 
 if 'data' not in st.session_state:
 	st.session_state['data'] = []
-	
-base_embeddings = OpenAIEmbeddings()
-llm_hyde = OpenAI()
-
-prompt_template = """Redacta un fragmento de un artículo científico para responder a la pregunta.
-Pregunta: {question}
-Respuesta:"""
-prompt = PromptTemplate(input_variables=["question"], template=prompt_template)
-llm_chain = LLMChain(llm=llm_hyde, prompt=prompt)
-
-embeddings = HypotheticalDocumentEmbedder(
-    llm_chain=llm_chain, base_embeddings=base_embeddings
-)
 
 os.environ['WEAVIATE_URL']="https://my-book-test-jr7qrzyj.weaviate.network"
 
-auth_config = weaviate.AuthApiKey(api_key=os.environ['WEAVIATE_API_KEY'])
+@st.cache_resource
+def get_chain():
+	base_embeddings = OpenAIEmbeddings()
+	llm_hyde = OpenAI()
+	
+	prompt_template = """Redacta un fragmento de un artículo científico para responder a la pregunta.
+	Pregunta: {question}
+	Respuesta:"""
+	prompt = PromptTemplate(input_variables=["question"], template=prompt_template)
+	llm_chain = LLMChain(llm=llm_hyde, prompt=prompt)
+	
+	embeddings = HypotheticalDocumentEmbedder(
+	    llm_chain=llm_chain, base_embeddings=base_embeddings
+	)
+	
+	
+	
+	auth_config = weaviate.AuthApiKey(api_key=os.environ['WEAVIATE_API_KEY'])
+	
+	client = weaviate.Client(url=os.environ['WEAVIATE_URL'], auth_client_secret=auth_config, additional_headers={
+	        "X-OpenAI-Api-Key": os.environ['OPENAI_API_KEY'], # Replace with your OpenAI key
+	        })
+	
+	vectordb = WeaviateLangChain(client=client,  index_name="ICC", text_key="content", embedding=embeddings)
+	
+	retriever = vectordb.as_retriever(search_kwargs={"k": 3})
+	
+	prompt=PromptTemplate(
+	    template="""Como médico cardiólogo especializado, te brindaré respuestas precisas y fundamentadas en el campo de la cardiología, basándome únicamente en la información proporcionada en el texto médico que me presentes. Mi objetivo es comportarme como un experto en cardiología y ofrecerte una asistencia confiable y precisa.
+	
+	No dudes en plantear cualquier pregunta relacionada con cardiología en función del contexto provisto, y estaré encantado de ayudarte y compartir mi conocimiento en este campo. Estoy comprometido a brindarte respuestas confiables y basadas en la evidencia médica presentada. En caso de desconocer la respuesta o no contar con información para responder la pregunta, diré 'No lo sé'.
+	----------------
+	{context}
+	----------------""",
+	    input_variables=["context"],
+	)
+	system_message_prompt = SystemMessagePromptTemplate(prompt=prompt)
+	
+	prompt=PromptTemplate(
+	    template="""{question}""",
+	    input_variables=["question"],
+	)
+	human_message_prompt = HumanMessagePromptTemplate(prompt=prompt)
+	
+	chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+	
+	condense_template = """Dada la siguiente conversación y una pregunta de seguimiento, reformula la pregunta de seguimiento para que sea una pregunta independiente.
+	
+	Historial del chat:
+	{chat_history}
+	Entrada de seguimiento: {question}
+	Pregunta independiente:"""
+	CONDENSE_QUESTION_PROMPT = PromptTemplate(template=condense_template, input_variables=["chat_history", "question"])
+	
+	llm = ChatOpenAI()
+	
+	question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
+	
+	llm2 = ChatOpenAI(temperature=0, verbose=True)
+	llm3 = ChatOpenAI(temperature=0, verbose=True, max_tokens=500)
+	question_generator = LLMChain(llm=llm2, prompt=CONDENSE_QUESTION_PROMPT)
+	doc_chain = load_qa_chain(llm3, chain_type="stuff", verbose=True)
+	
+	chain = ConversationalRetrievalChain(
+	    retriever=retriever,
+	    question_generator=question_generator,
+	    combine_docs_chain=doc_chain,
+	    verbose=True, return_source_documents=True
+	)
+	
+	chain.combine_docs_chain.llm_chain.prompt = chat_prompt
+    	return chain
 
-client = weaviate.Client(url=os.environ['WEAVIATE_URL'], auth_client_secret=auth_config, additional_headers={
-        "X-OpenAI-Api-Key": os.environ['OPENAI_API_KEY'], # Replace with your OpenAI key
-        })
+chain = get_chain()
 
-vectordb = WeaviateLangChain(client=client,  index_name="ICC", text_key="content", embedding=embeddings)
-
-retriever = vectordb.as_retriever(search_kwargs={"k": 3})
-
-prompt=PromptTemplate(
-    template="""Como médico cardiólogo especializado, te brindaré respuestas precisas y fundamentadas en el campo de la cardiología, basándome únicamente en la información proporcionada en el texto médico que me presentes. Mi objetivo es comportarme como un experto en cardiología y ofrecerte una asistencia confiable y precisa.
-
-No dudes en plantear cualquier pregunta relacionada con cardiología en función del contexto provisto, y estaré encantado de ayudarte y compartir mi conocimiento en este campo. Estoy comprometido a brindarte respuestas confiables y basadas en la evidencia médica presentada. En caso de desconocer la respuesta o no contar con información para responder la pregunta, diré 'No lo sé'.
-----------------
-{context}
-----------------""",
-    input_variables=["context"],
-)
-system_message_prompt = SystemMessagePromptTemplate(prompt=prompt)
-
-prompt=PromptTemplate(
-    template="""{question}""",
-    input_variables=["question"],
-)
-human_message_prompt = HumanMessagePromptTemplate(prompt=prompt)
-
-chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
-condense_template = """Dada la siguiente conversación y una pregunta de seguimiento, reformula la pregunta de seguimiento para que sea una pregunta independiente.
-
-Historial del chat:
-{chat_history}
-Entrada de seguimiento: {question}
-Pregunta independiente:"""
-CONDENSE_QUESTION_PROMPT = PromptTemplate(template=condense_template, input_variables=["chat_history", "question"])
-
-llm = ChatOpenAI()
-
-question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
-
-llm2 = ChatOpenAI(temperature=0, verbose=True)
-llm3 = ChatOpenAI(temperature=0, verbose=True, max_tokens=500)
-question_generator = LLMChain(llm=llm2, prompt=CONDENSE_QUESTION_PROMPT)
-doc_chain = load_qa_chain(llm3, chain_type="stuff", verbose=True)
-
-chain = ConversationalRetrievalChain(
-    retriever=retriever,
-    question_generator=question_generator,
-    combine_docs_chain=doc_chain,
-    verbose=True, return_source_documents=True
-)
-
-chain.combine_docs_chain.llm_chain.prompt = chat_prompt
-
-print(st.session_state)
 st.title("CardioBot :hospital:")
 col1, col2 = st.columns(2)
 with col1:
@@ -136,12 +142,9 @@ with col1:
 	                    #docs = retriever.get_relevant_documents(user_input)
 	                    if len(st.session_state.ai) == 0:
 	                        with get_openai_callback() as cb:
-	                        	response = chain({"question": user_input, "chat_history": []})
-	                        print("CB:", cb)
-	                        print("response", response)
+	                        response = chain({"question": user_input, "chat_history": []})
 	                        output = response['answer']
 	                        docs = response['source_documents']
-	                        print("docs", docs)
 	                        raw_string = ''
 	                        for d in range(len(docs)):
 	                        	raw_string += f'Extracto {d+1}:\n'
@@ -155,11 +158,7 @@ with col1:
 	                        st.session_state['generated'].append(output)   
 	                    elif len(st.session_state.ai) == 1:
 	                        chat_history = [(st.session_state['past'][-1], st.session_state['generated'][-1])]
-	                        print(chat_history)
-	                        with get_openai_callback() as cb:
-	                        	response = chain({"question": user_input, "chat_history": chat_history})
-	                        print("CB:", cb)
-	                        print("response", response)
+	                        response = chain({"question": user_input, "chat_history": chat_history})
 	                        output = response['answer']
 	                        docs = response['source_documents']   
 	                        raw_string = ''
@@ -174,11 +173,8 @@ with col1:
 	                        st.session_state.past.append(user_input)
 	                        st.session_state['generated'].append(output)
 	                    else:
-	                        chat_history = [(st.session_state['past'][-2], st.session_state['generated'][-2]), (st.session_state['past'][-1], st.session_state['generated'][-1])]
-	                        print(chat_history)	                        
-	                        with get_openai_callback() as cb:
-	                        	response = chain({"question": user_input, "chat_history": chat_history})
-	                        print("CB:", cb)
+	                        chat_history = [(st.session_state['past'][-2], st.session_state['generated'][-2]), (st.session_state['past'][-1], st.session_state['generated'][-1])]                
+	                        response = chain({"question": user_input, "chat_history": chat_history})
 	                        output = response['answer']
 	                        docs = response['source_documents']
 	                        raw_string = ''
