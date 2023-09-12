52cc466b0929a8e87f89ed1bbabd72fb95b128aa
Américo Valdazo <63754514+ValdazoAmerico@users.noreply.github.com>
2023-09-12 13:38:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -392,19 +392,18 @@ def clean_text(text):
   		return " ".join(filtered_words).strip()
 
 
-@st.cache_resource
-def get_chain():
-	auth_config = weaviate.AuthApiKey(api_key=os.environ['WEAVIATE_API_KEY'])
+
+auth_config = weaviate.AuthApiKey(api_key=os.environ['WEAVIATE_API_KEY'])
 	
-	client = weaviate.Client(url=os.environ['WEAVIATE_URL'], auth_client_secret=auth_config, additional_headers={
+client = weaviate.Client(url=os.environ['WEAVIATE_URL'], auth_client_secret=auth_config, additional_headers={
 	        "X-OpenAI-Api-Key": os.environ['OPENAI_API_KEY'], # Replace with your OpenAI key
 	        })
-	auth_config2 = weaviate.AuthApiKey(api_key=os.environ['WEAVIATE_API_KEY2'])
+auth_config2 = weaviate.AuthApiKey(api_key=os.environ['WEAVIATE_API_KEY2'])
 	
-	client2 = weaviate.Client(url=os.environ['WEAVIATE_URL2'], auth_client_secret=auth_config2, additional_headers={
+client2 = weaviate.Client(url=os.environ['WEAVIATE_URL2'], auth_client_secret=auth_config2, additional_headers={
 	        "X-OpenAI-Api-Key": os.environ['OPENAI_API_KEY'], # Replace with your OpenAI key
 	        })
-	retriever = WeaviateHybridSearchRetriever(
+retriever = WeaviateHybridSearchRetriever(
     	client=client,
     	index_name="Evicardio",
     	text_key="content",
@@ -412,18 +411,18 @@ def get_chain():
     	create_schema_if_missing=True,
 )
 
-	retriever2 = WeaviateHybridSearchRetriever(
+retriever2 = WeaviateHybridSearchRetriever(
     	client=client2,
     	index_name="Evicardio",
     	text_key="content",
     	attributes=[],
     	create_schema_if_missing=True,
 )
-	retriever.alpha = 0.25
+retriever.alpha = 0.25
 	#retriever.k=2
 	#retriever2.k=2
-	lotr = MergerRetriever(retrievers=[retriever, retriever2])
-	class CustomRetriever(BaseRetriever):
+lotr = MergerRetriever(retrievers=[retriever, retriever2])
+class CustomRetriever(BaseRetriever):
 		def _get_relevant_documents(
         self, query: str, *, run_manager: None
     ):
@@ -441,7 +440,7 @@ def get_chain():
 	custom_retriever = CustomRetriever()
 	
 	
-	prompt=PromptTemplate(
+prompt=PromptTemplate(
 	    template="""Actúas como un médico cardiólogo especializado. Tu tarea consiste en proporcionar respuestas precisas y fundamentadas en el campo de la cardiología, basándote únicamente en la información proporcionada en el texto médico que se te presente. Tu objetivo es comportarte como un experto en cardiología y ofrecer asistencia confiable y precisa.
 
 Debes responder solo a preguntas relacionadas con cardiología en función del contexto proporcionado. Estás comprometido a brindar respuestas confiables y basadas en la evidencia médica presentada. En caso de desconocer la respuesta o no contar con información para responder la pregunta, simplemente dirás 'No lo sé'. No intentarás inventar una respuesta.
@@ -455,17 +454,17 @@ Debes responder solo a preguntas relacionadas con cardiología en función del c
 """,
 	    input_variables=["context"],
 	)
-	system_message_prompt = SystemMessagePromptTemplate(prompt=prompt)
+system_message_prompt = SystemMessagePromptTemplate(prompt=prompt)
 	
-	prompt=PromptTemplate(
+prompt=PromptTemplate(
 	    template="""{question}""",
 	    input_variables=["question"],
 	)
-	human_message_prompt = HumanMessagePromptTemplate(prompt=prompt)
+human_message_prompt = HumanMessagePromptTemplate(prompt=prompt)
 	
-	chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
 	
-	condense_template = """Dada la siguiente conversación y una pregunta de seguimiento, reformula la pregunta de seguimiento para que sea una pregunta independiente.
+condense_template = """Dada la siguiente conversación y una pregunta de seguimiento, reformula la pregunta de seguimiento para que sea una pregunta independiente.
 	
 Conversación:
 {chat_history}
@@ -473,24 +472,23 @@ Conversación:
 Pregunta de seguimiento: {question}
 
 Pregunta independiente:"""
-	CONDENSE_QUESTION_PROMPT = PromptTemplate(template=condense_template, input_variables=["chat_history", "question"])
+CONDENSE_QUESTION_PROMPT = PromptTemplate(template=condense_template, input_variables=["chat_history", "question"])
 	
-	llm_question = ChatOpenAI()
+llm_question = ChatOpenAI()
 	
-	question_generator = LLMChain(llm=llm_question, prompt=CONDENSE_QUESTION_PROMPT)
+question_generator = LLMChain(llm=llm_question, prompt=CONDENSE_QUESTION_PROMPT)
 	
-	llm = ChatOpenAI(temperature=0, verbose=True, max_tokens=500, model='gpt-3.5-turbo-16k')
-	doc_chain = load_qa_chain(llm, chain_type="stuff", verbose=True)
+llm = ChatOpenAI(temperature=0, verbose=True, max_tokens=500, model='gpt-3.5-turbo-16k')
+doc_chain = load_qa_chain(llm, chain_type="stuff", verbose=True)
 	
-	chain = ConversationalRetrievalChain(
+chain = ConversationalRetrievalChain(
 	    retriever=custom_retriever,
 	    question_generator=question_generator,
 	    combine_docs_chain=doc_chain,
 	    verbose=True, return_source_documents=True
 	)
 	
-	chain.combine_docs_chain.llm_chain.prompt = chat_prompt
-	return chain
+chain.combine_docs_chain.llm_chain.prompt = chat_prompt
 	
 def check_password():
     """Returns `True` if the user had the correct password."""
@@ -521,7 +519,6 @@ def check_password():
         return True
 
 if check_password():
-		chain = get_chain()
 		#response = chain({"question": "que es el enalapril", "chat_history": []})
 		#print(response)
 	
